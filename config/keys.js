f69739b3fb299ea310df6e79340c125131d1a5de
Kaihua Tan <muleon2@gmail.com>
2019-10-05 10:56:44 -0700

    First Commit

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: "mongodb+srv://muleon2:Una1021Leo@cluster0-8f8tq.mongodb.net/admin?retryWrites=true&w=majority" 
+};
---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+//Bodyparser middleware
+app.use(
+    bodyParser.urlencoded({
+        extended: false
+    })
+);
+
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+const port = 4000; 
+
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
