d98699c5b59d3ec07b6f11e2644f3ada07d0b8c4
Kaihua Tan <muleon2@gmail.com>
2019-10-06 20:48:37 -0700

    part 1

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: "mongodb+srv://muleon2:Una1021Leo@cluster0-8f8tq.mongodb.net/admin?retryWrites=true&w=majority" 
+    mongoURI: "mongodb+srv://muleon2:Una1021Leo@cluster0-8f8tq.mongodb.net/admin?retryWrites=true&w=majority",
+    secretOrKey: "secret"
 };
